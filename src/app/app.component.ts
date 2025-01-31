410c9d73d1bb1d94a165a1a7dc591a38e4c97587
Gustavo Cereceres <gustavo.cereceres@unosquare.com>
2025-01-31 14:21:47 -0600

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NewTaskComponent } from './new-task/new-task.component';
 import { TaskListComponent } from './task-list/task-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NewTaskComponent, TaskListComponent],
+  imports: [RouterOutlet, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
